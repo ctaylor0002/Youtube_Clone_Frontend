19235e59c091b4ad71ffdee9e41af42caf314836
Charles Taylor <charles4312@comcast.net>
2022-11-27 01:17:17 -0500

    Added Related Videos To VideoPage + Allowed User To Click On those Videos

--- frontend/src/pages/VideoPage/VideoPage.js
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -1,35 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { KEY } from "../../API_Credentials"
+import { KEY } from "../../API_Credentials";
+import { Link } from 'react-router-dom';
 
 const VideoPage = (props) => {
     const {videoID} = useParams();
-    const [video, setVideo] = useState({});
+    const [relatedVideos, setRelatedVideos] = useState([]);
 
     useEffect(() => {
-      console.log(props)
-      // const clickVideo = async () => {
-      //   try {
-      //     let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&id=${videoID}&key=${KEY}`)
-      //     console.log(response)
-          
-      //     setVideo(response.data);
-      //     console.log(video);
-  
-      //   } catch (error) {
-      //     console.log(error);
-      //   }
-      // }
-        
-        console.log(video)
-      //clickVideo();
+      getRelatedVideos();
     }, [videoID])
 
-    
+    const getRelatedVideos = async () => {
+      try {
+            let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${videoID}&type=video&part=snippet&key=${KEY}`)
+            console.log(response)
+            setRelatedVideos(response.data.items);
+
+          } catch (error) {
+            console.log(error);
+          }
+    }
 
     return ( 
+      <div>
         <iframe src={`https://www.youtube.com/embed/${videoID}`} title='Test'/>
+        <div className='video-container'>
+          {relatedVideos &&
+          relatedVideos.map((relatedVideo) => (
+            <Link to={`/${relatedVideo.id.videoId}`} key={relatedVideo.id.videoId}>
+              <div className="video" >
+                <img src={relatedVideo.snippet.thumbnails.medium.url} alt={relatedVideo.snippet.description}/>
+                <p key={relatedVideo.id.videoId}>
+                  {relatedVideo.snippet.title} - {relatedVideo.snippet.channelTitle}
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+        
      );
 }
  
